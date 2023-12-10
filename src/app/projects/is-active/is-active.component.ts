c75d8f2f1b11be539dc5f15ba8a0bfa109767ac5
Divyansh Agarwal <dagarwal058@gmail.com>
2023-12-10 22:23:10 +0530

    isActive change

--- src/app/projects/is-active/is-active.component.ts
+++ src/app/projects/is-active/is-active.component.ts
@@ -8,9 +8,4 @@ import { Component } from '@angular/core';
 export class IsActiveComponent {
   numbers: number[] =  [1,2,3,4];
   current: number = 0;
-
-  changeActive(i : number)
-  {
-      this.current = i;
-  }
 }
